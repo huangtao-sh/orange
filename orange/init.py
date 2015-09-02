c2f02ef21ed7efd8d9694004273a11cc09319427
huangtao-sh <huangtao.jh@gmail.com>
2015-09-03 07:54:54 +0800

    add setup.py

--- orange/init.py
+++ orange/init.py
@@ -13,14 +13,58 @@ INITIAL_FILE='''# 项目：{project}
 # 邮件：{email}
 # 创建：{date:%Y-%m-%d}
 '''
+SETUP_FILE='''#!/usr/bin/env python3
+from setuptools import setup,find_packages
+from {prj_name}.__version__ import version
+setup(
+        name='{prj_name}',
+        version=version,
+        author='{author}',
+        author_email='{email}',
+        platforms='any',
+        description='{project}',
+        long_description='{project}',
+        entry_points={'console_scripts':[
+            # 'cmd_name=package:function',
+            ]},
+        packages=find_packages(exclude=['testing']),
+        license='GPL',
+        )
+'''
+TESTING_PKG='''from .test_sample import *
+'''
+TEST_SAMPLE='''import unittest
+class TestSample(unittest.TestCase):
+    def setUp(self)):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_sample(self):
+        a='this is a test.'
+        self.assertEqual(a,a)
+'''
+
 def py_init(project='',author='',email=''):
     prj_name=os.path.split(os.path.abspath('.'))[-1]
     ver_file='%s/__version__.py'%(prj_name)
     pkg_file='%s/__init__.py'%(prj_name)
+    # 生成setup.py文件
+    if not os.path.isfile('setup.py'):
+        stdlib.write_file('setup.py',SETUP_FILE.format(\
+            author=author,prj_name=prj_name,
+            project=project,email=email))
+        print('已生成setup.py')
+        
+    # 创建包目录
+    if not os.path.isdir(prj_name):
+        os.mkdir(prj_name)
+    # 生成初始化版本文件    
     if not os.path.isfile(ver_file):
         stdlib.write_file(ver_file,INITIAL_VERSION)
         print('当前程序版本初始化为：0.1')
-        
+    # 生成初始化包文件
     if not os.path.isfile(pkg_file):
         contents=INITIAL_FILE.format(project=project,
                                       author=author,
@@ -28,7 +72,14 @@ def py_init(project='',author='',email=''):
                                       date=datetime.datetime.now())
         stdlib.write_file(pkg_file,contents)
         print('已初始化包文件')
-    
+
+    if not os.path.isdir('testing'):
+        os.mkdir('testing')
+        stdlib.write_file(os.path.join('testing','__init__.py'),
+                          TESTING_PKG)
+        stdlib.write_file(os.path.join('testing','test_sample.py'),
+                          TESTING_SAMPLE)       
+        
 init_cmd={
     'proc':py_init,
     '-p --project':{
