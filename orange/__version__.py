6b3f0214a31e653cf70ee4c73154052ae5178c46
huangtao-sh <huangtao.jh@gmail.com>
2016-03-07 22:22:55 +0800

    modified setup.py

--- orange/__version__.py
+++ orange/__version__.py
@@ -1,1 +1,1 @@
-version="0.1.1a16"
+version="0.1.1a17"
--- setup.py
+++ setup.py
@@ -1,22 +1,15 @@
 #!/usr/bin/env python3
-from setuptools import setup,find_packages
-from orange import get_ver
+from stdlib._setup import setup
 console_scripts=['pytool=orange:main',
                  'sysinit=orange.sysinit:proc']
 scripts=['orange/pysetup.py','orange/pytest.py']
 setup(
         name='orange',
-        version=get_ver(),
-        author='Huang tao',
-        author_email='huangtao.jh@gmail.com',
         platforms='any',
         scripts=scripts,
         description='orange',
         long_description='orange',
         url='https://github.com/huangtao-sh/orange.git',
         entry_points={'console_scripts':console_scripts},
-        packages=find_packages(exclude=['testing']),
         license='GPL',
         )
-import stdlib.setup
-stdlib.setup.create_cmd(scripts)
