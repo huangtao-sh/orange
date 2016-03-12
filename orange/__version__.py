b4128f6ef65389e812118a8904b912fb0c9d4cde
huangtao-sh <huangtao.jh@gmail.com>
2016-03-12 18:36:57 +0800

    upgrade

--- orange/__version__.py
+++ orange/__version__.py
@@ -1,1 +1,1 @@
-version="0.1.1a24"
+version="0.1.1a25"
--- orange/pytest.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import sys
-sys.path.insert(0,'.')
-print(sys.path)
-from testing import *
-print(dir())
-def main():
-
-    unittest.main()
