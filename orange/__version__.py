0f4d37ac4b3a3d158095d2261a02321c7b169162
huangtao-sh <huangtao.jh@gmail.com>
2016-08-13 18:48:39 +0800

    upgrade

--- orange/__version__.py
+++ orange/__version__.py
@@ -1,1 +1,1 @@
-version="0.1.1a72"
+version="0.1.1b2"
--- orange/path.py
+++ orange/path.py
@@ -5,6 +5,7 @@
 # Email:huangtao.sh@icloud.com
 # 创建：2016-03-11 12:21
 # 修改：2016-04-13 21:01
+# 修改：2016-8-13 新增__iter__功能，和 extractall功能
 
 import pathlib
 import os
@@ -67,23 +68,28 @@ class Path(pathlib.Path):
         return self
 
     def read(self,*args,**kwargs):
+        '''以指定的参数读取文件'''
         with self.open(*args,**kwargs)as fn:
             return fn.read()
 
     def ensure(self,parents=True):
+        '''确保目录存在，如果目录不存在则直接创建'''
         if not self.exists():
             self.mkdir(parents=parents)            
         
     @property
     def lines(self):
+        '''按行读取文件'''
         return decode(self.read('rb'))
 
     @lines.setter
     def lines(self,lines):
+        '''按行写入文件'''
         self.write(*lines)
         
     def write(self,*lines,text=None,data=None,encoding='utf8',
               parents=False):
+        '''写文件'''
         if parents:
             self.parent.ensure()
         if lines:
@@ -93,8 +99,65 @@ class Path(pathlib.Path):
         if data:
             with self.open('wb')as fn:
                 fn.write(data)
+                
+    def iter_sheets(self):
+        '''如果指定的文件为excel文件，则可以迭代读取本文件的数据。
+        返回：表的索引、表名、数据'''
+        import xlrd
+        book=xlrd.open_workbook(filename=str(self))
+        for index,sheet in enumerate(book.sheets()):
+            yield index,sheet.name,sheet._cell_values
 
+    @property
+    def xmlroot(self):
+        '''如果指定的文件为xml文件，则返回本文件的根元素'''
+        import lxml.etree
+        return lxml.etree.parse(str(self)).getroot()
+        
+    def __iter__(self):
+        '''根据文件的不同，迭代返回不同的内容。支持如下文件：
+        1、文本文件，按行返回文本
+        2、Excel文件，返回表索引、表名及表中数据
+        3、目录，则返回本目录下所有文件
+        4、del文件，按行返回数据。
+        5、csv文件，按行返回数据。
+        '''
+        if self.is_dir():
+            return self.glob('*')
+        suffix=self.lsuffix
+        if suffix.startswith('.xls'):
+            return self.iter_sheets()
+        elif suffix=='.xml':
+            return self.xmlroot.iterchildren()
+        elif suffix=='.del':
+            import re
+            none_pattern=re.compile(",(?=,)")
+            for line in self.lines:
+                if line:
+                    yield eval(none_pattern.sub(",None",line))
+        elif suffix=='.csv':
+            for line in self.lines:
+                if line:
+                    yield line.split(',')
+
+    def extractall(self,path='.',members=None):
+        '''如本文件为tar打包文件，则解压缩至指定目录'''
+        import tarfile
+        path=str(Path(path))
+        tarfile.open(str(self),'r').extractall(path,members)
+        
+    @property
+    def lsuffix(self):
+        '''返回小写的扩展名'''
+        return self.suffix.lower()
+    
+    @property
+    def pname(self):
+        '''返回不带扩展名的文件名'''
+        return self.with_suffix("").name
+    
     def rmtree(self):
+        '''删除整个目录'''
         import shutil
         shutil.rmtree(str(self))
         
