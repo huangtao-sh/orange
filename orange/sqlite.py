ef425de93f778ef5fbdd4f7d807224be61dbbe8c
huangtao-sh <huangtao.jh@gmail.com>
2018-07-30 21:34:36 +0800

    formated

--- orange/sqlite.py
+++ orange/sqlite.py
@@ -11,7 +11,7 @@ from orange import Path, is_dev, info
 
 
 __all__ = 'db_config', 'connect', 'execute', 'executemany',\
-    'executescript', 'find', 'findone', 'find_','findone_'
+    'executescript', 'find', 'findone', 'find_', 'findone_'
 
 ROOT = Path('~/OneDrive')
 ROOT = ROOT / ('testdb' if is_dev() else 'db')
@@ -37,7 +37,7 @@ def db_config(database: str, **kw):
 
 
 class connect():
-    def __init__(self,database: str=None, **kw):
+    def __init__(self, database: str=None, **kw):
         if not database:
             kw = _db_config.copy()
             database = kw.pop('database')
@@ -47,16 +47,16 @@ class connect():
                 db = ROOT/db
             db = db.with_suffix('.db')
             database = str(db)
-        self._db=database
-        self._kw=kw    
+        self._db = database
+        self._kw = kw
 
     def __enter__(self):
         connection = sqlite3.connect(self._db, **self._kw)
         _conn_stack.push(connection)
         return connection
 
-    def __exit__(self,exc_type, exc_val, exc_tb):
-        conn=_conn_stack.pop()
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        conn = _conn_stack.pop()
         if exc_type:
             conn.rollback()
         else:
@@ -69,8 +69,8 @@ class connect():
         _conn_stack.push(connection)
         return connection
 
-    async def __aexit__(self,exc_type, exc_val, exc_tb):
-        conn=_conn_stack.pop()
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        conn = _conn_stack.pop()
         if exc_type:
             await conn.rollback()
         else:
@@ -91,27 +91,30 @@ def executemany(sql, params=None):
     params = params or []
     return conn.executemany(sql, params)
 
-def find(sql, params=None,multi=True):
-    con_=_conn_stack.top
-    if not isinstance(con_,sqlite3.Connection):
-        con_=con_._conn
-    params=params or []
-    cursor=con_.execute(sql,params)
+
+def find(sql, params=None, multi=True):
+    con_ = _conn_stack.top
+    if not isinstance(con_, sqlite3.Connection):
+        con_ = con_._conn
+    params = params or []
+    cursor = con_.execute(sql, params)
     if multi:
         return cursor.fetchall()
     else:
         return cursor.fetchone()
 
+
 def findone(sql, params=None):
-    return find(sql,params,multi=False)
+    return find(sql, params, multi=False)
 
 
-async def findone_(sql,params=None):
-    cursor=await execute(sql,params)
+async def findone_(sql, params=None):
+    cursor = await execute(sql, params)
     return await cursor.fetchone()
 
-async def find_(sql,params=None):
-    cursor=await execute(sql,params)
+
+async def find_(sql, params=None):
+    cursor = await execute(sql, params)
     return await cursor.fetchall()
 
 if __name__ == '__main__':
@@ -148,17 +151,16 @@ if __name__ == '__main__':
         print('-'*20)
         for b in find('select * from abc limit 2'):
             print(*b)
+
     async def _():
         async with connect():
             await execute('insert into abc values(7,8)')
-            data=[(x,x+20)for x in range(20) ]
-            await executemany('insert into abc values(?,?)',data)
-            d=find('select * from abc')
-            for row in d :
+            data = [(x, x+20)for x in range(20)]
+            await executemany('insert into abc values(?,?)', data)
+            d = find('select * from abc')
+            for row in d:
                 print(*row)
-            b=await findone_('select * from abc')
+            b = await findone_('select * from abc')
             print(*b)
     print('*'*20)
     run(_())
-
-    
