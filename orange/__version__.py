8cf9de5f7823287fa3719d7ff39f1dcb2ec4c39a
huangtao-sh <huangtao.jh@gmail.com>
2018-07-31 15:14:47 +0800

    upgrade

--- orange/__version__.py
+++ orange/__version__.py
@@ -1,1 +1,1 @@
-version = "0.1.6a47"
+version = "0.1.6a48"
--- testing/testsqlite.py
+++ testing/testsqlite.py
@@ -1,28 +1,35 @@
 import unittest
-from orange.sqlite import connect
+from orange.sqlite import connect, execute, executemany, executescript, find, findone, db_config
 from orange import Path
+from orange.coroutine import run
+
+db_config(':memory:')
 
 
 class TestSqlite(unittest.TestCase):
 
     def tearDown(self):
-        #if Path('test.db'):
+        # if Path('test.db'):
         #    Path('test.db').unlink()
         pass
+
     def test_sqlite(self):
-        with connect('test.db')as db:
+        with connect():
             rows = ('make', 'you', 'good')
-            with db.trans():
-                db.execute('create table if not exists test(a,b,c)')
-                db.execute('insert into test values(?,?,?)', rows)
-            result = db.findone('select * from test')
+            execute('create table if not exists test(a,b,c)')
+            execute('insert into test values(?,?,?)', rows)
+            result = findone('select * from test')
             self.assertListEqual(list(rows), list(result))
 
     def test_query(self):
-        rows=[('a','b',i) for i in range(10)]
-        with connect('test.db')as db:
-            with db.trans():
-                db.execute('delete from test')
-                db.executemany('insert into test values(?,?,?)',rows)
-            ab=db.find('select * from test')
-            self.assertEqual(len(ab),10)
+        rows = [('a', 'b', i) for i in range(10)]
+        async def _():
+            async with connect():
+                await execute('create table if not exists test(a,b,c)')
+                await executemany('insert into test values(?,?,?)', rows)
+                await executescript('insert into test values("a","b",2)')
+                ab = await find('select * from test')
+                self.assertEqual(len(ab), 11)
+                d = await findone('select * from test limit 1')
+                self.assertEqual(d[0],'a')
+        run(_())
