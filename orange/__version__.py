2da8e41107618cf1ce5fe421267eb14fc7cd479a
huangtao-sh <huangtao.jh@gmail.com>
2019-08-04 11:13:30 +0800

    upgrade

--- orange/__version__.py
+++ orange/__version__.py
@@ -1,1 +1,1 @@
-version = "0.1.8b26"
+version = "0.1.8b27"
--- test2.py
+++ test2.py
@@ -6,23 +6,23 @@ a = Path(r'C:\Users\huangtao\OneDrive\工作\参数备份\运营管理2019-06\fh
 
 @filterer
 def _fil(row):
-    print('filter1', row)
+    print('filter1')
     return row[0].startswith('331')
 
 
 @filterer
 def fil2(row):
-    print('filter2', row)
+    print('filter2')
     return True
 
 
 @converter
 def conv(row):
-    print('converter', row)
+    print('converter')
     return row
 
 
 data = a.iter_csv(_fil, fil2, conv, encoding='gbk', errors='ignore')
 
 for i, r in zip(range(20), data):
-    print(*r)
+    ...
