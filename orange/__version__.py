eb7292720dd52feac60b7753a482bdbe9875ce02
huangtao-sh <huangtao.jh@gmail.com>
2019-12-01 21:07:18 +0800

    upgrade

--- orange/__version__.py
+++ orange/__version__.py
@@ -1,1 +1,1 @@
-version = "0.1.8b58"
+version = "0.1.8b59"
--- orange/xlsx.py
+++ orange/xlsx.py
@@ -1,4 +1,3 @@
-
 # 项目：Excel写入模块封装
 # 模块：xlsxwriter的封装
 # 作者：黄涛
@@ -15,26 +14,47 @@ from xlsxwriter.worksheet import convert_cell_args, convert_column_args, convert
     xl_col_to_name
 from functools import partial
 
-Pattern = R/r'([A-Z]{1,2})(\d*)([:_]([A-Z]{1,2})(\d*))?'
-Row = R/r'(\{([+-]?\d+)\})'
+Pattern = R / r'([A-Z]{1,2})(\d*)([:_]([A-Z]{1,2})(\d*))?'
+Row = R / r'(\{([+-]?\d+)\})'
 
-DefaultFormat = (('currency', {'num_format': '#,##0.00'}),
-                 ('rate', {'num_format': '0.0000%'}),
-                 ('title', {'font_name': '黑体', 'font_size': 16,
-                            'align': 'center'}),
-                 ('h2', {'font_name': '黑体', 'font_size': 12,
-                         'align': 'center'}),
-                 ('mh2', {'font_name': '黑体', 'font_size': 12, 'text_wrap': True,
-                          'align': 'center', 'valign': 'vcenter'}),
-                 ('percent', {'num_format': '0.00%'}),
-                 ('date', {'num_format': 'yyyy-mm-dd'}),
-                 ('normal', {'text_wrap': True, 'valign': 'vcenter'}),
-                 ('time', {'num_format': 'hh:mm:ss'}),
-                 ('number', {'num_format': '#,##0'}),
-                 ('header', {'font_name': '黑体',
-                             'font_size': 12, 'align': 'center'}),
-                 ('datetime', {'num_format': 'yyyy-mm-dd hh:mm:ss'}),
-                 ('timestamp', {'num_format': 'yyyy-mm-dd hh:mm:ss.0'}))
+DefaultFormat = (('currency', {
+    'num_format': '#,##0.00'
+}), ('rate', {
+    'num_format': '0.0000%'
+}), ('title', {
+    'font_name': '黑体',
+    'font_size': 16,
+    'align': 'center'
+}), ('h2', {
+    'font_name': '黑体',
+    'font_size': 12,
+    'align': 'center'
+}), ('mh2', {
+    'font_name': '黑体',
+    'font_size': 12,
+    'text_wrap': True,
+    'align': 'center',
+    'valign': 'vcenter'
+}), ('percent', {
+    'num_format': '0.00%'
+}), ('date', {
+    'num_format': 'yyyy-mm-dd'
+}), ('normal', {
+    'text_wrap': True,
+    'valign': 'vcenter'
+}), ('time', {
+    'num_format': 'hh:mm:ss'
+}), ('number', {
+    'num_format': '#,##0'
+}), ('header', {
+    'font_name': '黑体',
+    'font_size': 12,
+    'align': 'center'
+}), ('datetime', {
+    'num_format': 'yyyy-mm-dd hh:mm:ss'
+}), ('timestamp', {
+    'num_format': 'yyyy-mm-dd hh:mm:ss.0'
+}))
 
 
 def Header(header: str, width=None, format=None, **kw):
@@ -57,10 +77,11 @@ class Book(Workbook):
     ''' 对Xlsxwriter模块进一步进行封装'''
 
     def __init__(self, filename=None, formats=None, **kw):
-        filename = str(Path(filename))
+        if isinstance(filename(Path, str)):
+            filename = str(Path(filename))
         super().__init__(filename, **kw)
-        self._worksheet = None    # 设置当前的工作表为空
-        self._worksheets = {}     # 设置当前的工作表清单为空
+        self._worksheet = None  # 设置当前的工作表为空
+        self._worksheets = {}  # 设置当前的工作表清单为空
         self._formats = {}
         if formats:
             self.add_formats(formats)
@@ -81,9 +102,10 @@ class Book(Workbook):
             self.add_format(property, name)
 
     def set_widths(self, widths):
-        [self.set_columns(col, width=width)for col, width in widths.items()]
+        [self.set_columns(col, width=width) for col, width in widths.items()]
 
-    def set_columns(self, *columns, width=None, cell_format=None, options=None):
+    def set_columns(self, *columns, width=None, cell_format=None,
+                    options=None):
         ''' 设置当前工作表的列属性，允许同时设置多个，使用方法如下：
         book.set_columns('A:C','E:D','G:H',width=12)
         '''
@@ -93,7 +115,7 @@ class Book(Workbook):
 
     def newline(self):
         '''换行'''
-        return self+1
+        return self + 1
 
     @property
     def worksheet(self):
@@ -111,20 +133,24 @@ class Book(Workbook):
         self._worksheet = worksheet
 
     @convert_range_args
-    def write(self, first_row, first_col, last_row=None, last_col=None,
-              value=None, cell_format=None):
+    def write(self,
+              first_row,
+              first_col,
+              last_row=None,
+              last_col=None,
+              value=None,
+              cell_format=None):
         if isinstance(cell_format, str):
             cell_format = self._formats.get(cell_format)
-        if(last_row is None)or(first_row == last_row and
-                               first_col == last_col):
+        if (last_row is None) or (first_row == last_row
+                                  and first_col == last_col):
             if isinstance(value, (tuple, list)):
-                self.worksheet.write_row(first_row, first_col,
-                                         value, cell_format)
+                self.worksheet.write_row(first_row, first_col, value,
+                                         cell_format)
             else:
                 if isinstance(value, str) and value.startswith('='):
-                    value = Row/value % self._convert   # 使用正则表达式替换
-                self.worksheet.write(first_row, first_col, value,
-                                     cell_format)
+                    value = Row / value % self._convert  # 使用正则表达式替换
+                self.worksheet.write(first_row, first_col, value, cell_format)
         else:
             self.worksheet.merge_range(first_row, first_col, last_row,
                                        last_col, value, cell_format)
@@ -140,11 +166,11 @@ class Book(Workbook):
                 self.worksheet.write_row(range, value, format)
             else:
                 if isinstance(value, str) and value.startswith('='):
-                    value = Row/value % self._convert   # 使用正则表达式替换
+                    value = Row / value % self._convert  # 使用正则表达式替换
                 self.worksheet.write(range, value, format)
 
     def _convert(self, match):
-        return str(int(match.groups()[1])+self.row)
+        return str(int(match.groups()[1]) + self.row)
 
     def __add__(self, val):
         '''向前移动当前行'''
@@ -169,7 +195,7 @@ class Book(Workbook):
     def __setitem__(self, name, val):
         '''向指定行写入数据'''
         if not isinstance(val, tuple):
-            val = (val,)
+            val = (val, )
         if isinstance(name, str):
             match = Pattern.fullmatch(name)
             if match:
@@ -185,10 +211,10 @@ class Book(Workbook):
             else:
                 raise Exception('单元格格式不正确')
         elif isinstance(name, int):
-            self.write(self.row-1, name, None, None, *val)
+            self.write(self.row - 1, name, None, None, *val)
         elif isinstance(name, tuple):
             args = list(name)
-            args.extend([None]*4)
+            args.extend([None] * 4)
             self.write(*args[:4], *val)
 
     def __setattr__(self, name, val):
@@ -209,8 +235,16 @@ class Book(Workbook):
         return self.worksheet.table
 
     @convert_range_args
-    def set_border(self, first_row, first_col, last_row, last_col,
-                   left=None, right=None, bottom=None, top=None, border=2,
+    def set_border(self,
+                   first_row,
+                   first_col,
+                   last_row,
+                   last_col,
+                   left=None,
+                   right=None,
+                   bottom=None,
+                   top=None,
+                   border=2,
                    inner=1):
         self.worksheet._check_dimensions(first_row, first_col)
         self.worksheet._check_dimensions(last_row, last_col)
@@ -229,10 +263,10 @@ class Book(Workbook):
             kw['bottom'] = bottom if r == last_row else inner
             kw['left'] = left if c == first_col else inner
             kw['right'] = right if c == last_col else inner
-            name = ''.join([str(kw[name]) for name in
-                            'left top right bottom'.split()])
+            name = ''.join(
+                [str(kw[name]) for name in 'left top right bottom'.split()])
             if fmt and hasattr(fmt, 'name'):
-                name = name+'-'+fmt.name
+                name = name + '-' + fmt.name
             if not name in self._formats:
                 if fmt and hasattr(fmt, 'properties'):
                     a = fmt.properties.copy()
@@ -243,17 +277,26 @@ class Book(Workbook):
             else:
                 new_fmt = self._formats.get(name)
             row[c] = cell._replace(format=new_fmt)
-        [_replace(r, c)for r in range(first_row, last_row+1)
-         for c in range(first_col, last_col+1)]
+
+        [
+            _replace(r, c) for r in range(first_row, last_row + 1)
+            for c in range(first_col, last_col + 1)
+        ]
 
     def add_table(self, pos, sheet=None, worksheet=None, data=None, **kw):
         worksheet = sheet or worksheet
         self._add_table(pos, worksheet=worksheet, data=data, **kw)
 
     @convert_range_args
-    def _add_table(self, first_row, first_col, last_row, last_col,
-                   worksheet=None, header_format='header',
-                   data=None, **kwargs):
+    def _add_table(self,
+                   first_row,
+                   first_col,
+                   last_row,
+                   last_col,
+                   worksheet=None,
+                   header_format='header',
+                   data=None,
+                   **kwargs):
         '''添加图表，如sheet为空，则使用默认的工作表'''
         if worksheet:
             self.worksheet = worksheet
@@ -265,8 +308,8 @@ class Book(Workbook):
             for idx, column in enumerate(columns):
                 if 'width' in column:
                     self.set_columns("{0}:{0}".format(
-                        xl_col_to_name(idx+first_col)),
-                        width=column.get('width'))
+                        xl_col_to_name(idx + first_col)),
+                                     width=column.get('width'))
                 if 'format' in column or 'header_format' in column:
                     new_column = column.copy()
                     format = column.get("format")
@@ -285,22 +328,28 @@ class Book(Workbook):
                 for column in new_columns:
                     column['header_format'] = header_format
             kwargs['columns'] = new_columns
-            last_col = first_col+len(columns)-1
+            last_col = first_col + len(columns) - 1
         if data:
             if not isinstance(data, (tuple, list)):
                 data = tuple(data)
-            last_row = first_row+len(data)
+            last_row = first_row + len(data)
             if kwargs.get('total_row', False):
                 last_row += 1
             kwargs['data'] = data
-        self.worksheet.add_table(first_row, first_col,
-                                 last_row, last_col, kwargs)
+        self.worksheet.add_table(first_row, first_col, last_row, last_col,
+                                 kwargs)
 
 
 if __name__ == '__main__':
     with Book('test.xlsx') as book:
         book.worksheet = 'test1'
-        columns = [{'header': 'title', 'format': 'currency', 'width': 12},
-                   {'header': 'name', 'format': 'percent'}]
+        columns = [{
+            'header': 'title',
+            'format': 'currency',
+            'width': 12
+        }, {
+            'header': 'name',
+            'format': 'percent'
+        }]
         data = [('a', 'b'), (1, 2)]
         book.add_table('A1', data=data, columns=columns)
