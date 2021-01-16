5b29d87ddb1a56a0ac390d960b1b2d708dfd9a8d
huangtao-sh <huangtao.jh@gmail.com>
2021-01-16 21:52:32 +0800

    upgrade

--- orange/__version__.py
+++ orange/__version__.py
@@ -1,1 +1,1 @@
-version = "0.1.9a39"
+version = "0.1.9a40"
--- test.py
+++
@@ -1,5 +0,0 @@
-from orange.mail import config, get_conf
-
-config(host='smtp.163.com', user='hunto@163.com', passwd='Huangtao1978')
-
-print(get_conf())
